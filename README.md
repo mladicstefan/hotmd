a8acd05ec4c2b8e308dd5272b56fcbced8638ded
mladicstefan <mladicstefan6@gmail.com>
2025-08-04 11:27:13 +0200

    client.lua

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Transpile your Markdown with custom fromatting!
 
-Work in Progressss
+### Steps to run:
+1. Build.sh --copies client.lua into ~/.config/nvim/lua/ and autocleansup
+2. Inside neovim run :source ~/.config/nvim/lua/client.lua WHILE THE build is running
+3. Backend is work in progress
--- client.lua
+++ client.lua
@@ -1,2 +1,83 @@
-local M = {}
+local socket = nil
+local connected =false
+
+local uv = vim.loop --tcp server
+local config = {
+  HOST = "127.0.0.1",
+  PORT = 6969
+}
+
+local function connect()
+  if connected then return end
+
+  socket = uv.new_tcp()
+  if not socket then
+      print("Failed to create TCP socket")
+      return
+  end
+
+  socket:connect(config.HOST, config.PORT, function (err)
+    if err then
+      print("Failed to establish connection with server...")
+      connected = false
+      return
+    end
+    connected = true
+    print("connected to server @ " ..config.HOST.. ":" ..config.PORT)
+  end)
+end
+
+local function send_keystroke(key)
+  if not connected or not socket then
+    return
+  end
+  socket:write(key, function(err)
+    if err then
+      print("Failed to send data...", err)
+      connected = false
+    end
+  end)
+end
+
+local function get_buffer_content()
+  local lines = vim.api.nvim_buf_get_lines(0,0,-1,false)
+  return table.concat(lines, '\n')
+end
+
+local function on_key(key)
+  --only send in insert mode
+  local mode = vim.api.nvim_get_mode().mode
+  if mode ~= "i" then return end
+
+  print("Key pressed:", vim.inspect(key), "Mode:", vim.api.nvim_get_mode().mode)
+  --TODO: FILTER KEYS 
+  if key == "\27" then return end
+
+  print("Sending key to server:", vim.inspect(key))
+  send_keystroke(key)
+end
+
+local function disconnect()
+  if socket then
+    socket:close()
+    socket = nil
+  end
+  connected = false
+  print("Disconnected...")
+end
+
+local function init()
+  connect()
+
+  vim.on_key(on_key)
+  --cleanup on exit 
+  -- vim.api.nvim_create_autocmd("VimLeavePre",{
+  --   callback = disconnect()
+  -- })
+end
+
+init()
+
+--expose user cleanup
+vim.api.nvim_create_user_command("KeystrokeDisconnect", disconnect, {})
 
