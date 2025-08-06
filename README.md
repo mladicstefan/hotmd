846d1d024eaea010ad544e0bae5f3c4aee6eaec1
mladicstefan <mladicstefan6@gmail.com>
2025-08-06 22:35:12 +0200

    working socket

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Transpile your Markdown with custom fromatting!
 
+
 ### Steps to run:
-1. Build.sh --copies client.lua into ~/.config/nvim/lua/ and autocleansup
+1. Build.sh --copies client.lua into ~/.config/nvim/lua/ and automatically cleans up
 2. Inside neovim run :source ~/.config/nvim/lua/client.lua WHILE THE build is running
 3. Backend is work in progress
-
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
-use std::io;
-use tokio::{io::AsyncReadExt, net::TcpListener};
+use tokio::io::AsyncReadExt;
+use tokio::net::TcpListener;
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("127.0.0.1:6969").await?;
 
-    let mut buff: Vec<u8> = Vec::new();
-
-    let (mut socket, _) = listener.accept().await?;
     loop {
-        let incoming = socket.read(&mut buff).await?;
-        println!("{:?}", incoming);
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    Ok(0) => return,
+                    Ok(n) => println!("{:?}", n),
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+            }
+        });
     }
 }
